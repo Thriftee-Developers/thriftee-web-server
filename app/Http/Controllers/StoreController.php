72548b46e64d7db7aa6c0bb90ec567619e02fe1b
Emerson Dalwampo <78949883+sncrr@users.noreply.github.com>
2022-02-13 23:07:36 +0800

    Revert "Create New Server"
    This reverts commit 03fb2ecbe6846cfb1fc85cfce08dbfcdc48e60e3.

---
+++ app/Http/Controllers/StoreController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Store;
+use Illuminate\Support\Facades\Hash;
+
+class StoreController extends Controller
+{
+    //
+    function addStore(Request $req)
+    {
+        $store = new Store;
+        $store -> uuid = $req -> input('uuid');
+        $store -> store_name = $req -> input('store_name');
+        $store -> image_uri = $req -> input('image_uri');
+        $store -> description = $req -> input('description');
+
+        $store -> username = $req -> input('username');
+        $store -> password = Hash::make($req -> input('password'));
+        $store -> save();
+        return $store;
+    }
+}
