20e52d95f4d4c018591428caff0fe76de22221d4
Emerson Dalwampo <78949883+sncrr@users.noreply.github.com>
2022-02-15 17:10:33 +0800

    Update StoreController.php

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -19,6 +19,7 @@ class StoreController extends Controller
         $store = new Store;
         $store -> uuid = $req -> input('uuid');
         $store -> store_id = $req -> input('store_id');
+        $store -> store_name = $req -> input('store_name');
         $store -> contact_no = $req -> input('contact_no');
         $store -> email = $req -> input('email');
         $store -> province = $req -> input('province');
