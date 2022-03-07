65edbe70054f0ed44f0a97850b74face358b1270
Emerson Dalwampo <78949883+sncrr@users.noreply.github.com>
2022-03-07 16:04:55 +0800

    Update MediaController.php

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -22,11 +22,12 @@ class MediaController extends Controller
             if(!empty($files)) {
                 $paths = array();
                 foreach($files as $file) {
-                    $result = Storage::disk([
-                        public_path('storage') => storage_path('app/public'),
-                        public_path('images') => storage_path('app/images')
-                    ])
-                    ->put($file->getClientOriginalName(), file_get_contents($file));
+                    $result = $file->storeAs($folder, $file->getClientOriginalName());
+                    // $result = Storage::disk([
+                    //     public_path('storage') => storage_path('app/public'),
+                    //     public_path('images') => storage_path('app/images')
+                    // ])
+                    // ->put($file->getClientOriginalName(), file_get_contents($file));
 
                     $paths[] = $result;
                 }
