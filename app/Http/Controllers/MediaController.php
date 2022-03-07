12abf2227913c977f24ec99b4e11d95dcb762b29
Emerson Dalwampo <78949883+sncrr@users.noreply.github.com>
2022-03-07 15:20:51 +0800

    Update MediaController.php

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -29,7 +29,7 @@ class MediaController extends Controller
             }
         }
         else {
-            $paths = 'No files';
+            $paths = $req->file;
         }
 
         return $paths;
