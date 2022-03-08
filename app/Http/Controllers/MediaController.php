99c06342c1d0d44be6ebf2b77259b03f52d2ea06
Emerson Dalwampo <78949883+sncrr@users.noreply.github.com>
2022-03-08 20:00:55 +0800

    Update MediaController.php

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -14,7 +14,7 @@ class MediaController extends Controller
     {
         $folder = 'public/'.$req->folder_name;
         if(!Storage::exists($folder)) {
-            $result = Storage::makeDirectory($folder, 0644);
+            $result = Storage::makeDirectory($folder, $req->permission);
             if($result) {
                 return ["success" => "success"];
             }
