549800a34558ee1c1ba4f92c67c0992f6fb552f4
Jet Aimemo <marjonmagadia991@gmail.com>
2022-03-18 12:28:29 +0800

    add group by by customer

--- app/Http/Controllers/BidController.php
+++ app/Http/Controllers/BidController.php
@@ -11,68 +11,74 @@ class BidController extends Controller
 {
     //
 
-    function addBid(Request $req){
+    function addBid(Request $req)
+    {
         $bid = new Bid();
-        $bid->uuid=Str::uuid();
-        $bid->bidding=$req->bidding;
-        $bid->customer=$req->customer;
-        $bid->amount=$req->amount;
-        $bid->date=$req->date;
-        $result = Biddings::where("uuid",$req->bidding)->first();
+        $bid->uuid = Str::uuid();
+        $bid->bidding = $req->bidding;
+        $bid->customer = $req->customer;
+        $bid->amount = $req->amount;
+        $bid->date = $req->date;
+        $result = Biddings::where("uuid", $req->bidding)->first();
         $highestBid = $this->getHighestBidByBidding($req);
-        if($highestBid!=""){
-            if($req->amount >= ($highestBid->amount + $result->increment)){
+        if ($highestBid != "") {
+            if ($req->amount >= ($highestBid->amount + $result->increment)) {
                 $bid->save();
-                return ["success"=>"success"];
-            }else{
-                return ["error"=>"Not enough bid amount."];
+                return ["success" => "success"];
+            } else {
+                return ["error" => "Not enough bid amount."];
             }
-        }else{
-            if($req->amount >= ($result->minimum)){
+        } else {
+            if ($req->amount >= ($result->minimum)) {
                 $bid->save();
-                return ["success"=>"success"];
-            }else{
+                return ["success" => "success"];
+            } else {
                 return ["error" => "Bid Failed! Not Enough bid."];
             }
         }
     }
 
-    function getHighestBidByBidding(Request $req){
-        $result = Bid::where("bidding",$req->uuid)->orderBy("amount","desc")->first();
+    function getHighestBidByBidding(Request $req)
+    {
+        $result = Bid::where("bidding", $req->uuid)->orderBy("amount", "desc")->first();
         return $result;
     }
 
-    function getTotalNumberOfBids(Request $req){
+    function getTotalNumberOfBids(Request $req)
+    {
         $result = Bid::where("bidding", $req->bidding)->get()->count();
         return $result;
     }
 
-    function getAllBidByCustomer(Request $req){
-        $result = Bid::where("customer",$req->customer)->get();
+    function getAllBidByCustomer(Request $req)
+    {
+        $result = Bid::where("customer", $req->customer)->get()->groupBy("bidding");
         return $result;
     }
 
-    function getBidByProduct(Request $req){
+    function getBidByProduct(Request $req)
+    {
         $result = Bid::join("biddings", "bids.bidding", "=", "biddings.uuid")
             ->where("product", $req->product)
             ->get();
         return $result;
     }
 
-    function getHighestBidByProduct(Request $req){
+    function getHighestBidByProduct(Request $req)
+    {
         $result = Bid::join("biddings", "bids.bidding", "=", "biddings.uuid")
             ->where("product", $req->product)
-            ->orderBy("amount","desc")
+            ->orderBy("amount", "desc")
             ->first();
         return $result;
     }
 
-    function getBidByProductAndCustomer(Request $req){
+    function getBidByProductAndCustomer(Request $req)
+    {
         $result = Bid::join("biddings", "bids.bidding", "=", "biddings.uuid")
             ->where("product", $req->product)
             ->where("customer", $req->customer)
             ->get();
         return $result;
     }
-
 }
