685406352a00a46182e7185d0097f9f75f37ac29
Emerson Dalwampo <78949883+sncrr@users.noreply.github.com>
2022-03-25 12:21:08 +0800

    Update BiddingController.php

--- app/Http/Controllers/BiddingController.php
+++ app/Http/Controllers/BiddingController.php
@@ -80,7 +80,7 @@ class BiddingController extends Controller
 
             $hoursdiff = round(($current_time - $end_time) / 3600);
             //48 hrs = 2 days
-            $winnerIndex = round($hoursdiff / 48, 1);
+            $winnerIndex = round($hoursdiff / 48, 0);
 
             $bids = Bid::where('bidding', $req->bidding)
                 ->orderBy('date', 'desc')
