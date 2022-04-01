a02b1b2231cd976690a57d8abdcb7a18179e89f0
Jet Aimemo <marjonmagadia991@gmail.com>
2022-04-02 05:27:36 +0800

    u - message

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,7 +38,7 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
-                'host' => '194.163.35.34',
+                'host' => '127.0.0.1',
                 'port' => "6001",
                 'scheme' => 'http'
             ],
