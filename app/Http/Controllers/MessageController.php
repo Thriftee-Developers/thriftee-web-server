14a08cb901556a6b7bee59acc5171ffea6ce1488
Marjon Magadia <marjonmagadia991@gmail.com>
2022-04-08 21:50:20 +0800

    u - add sync message

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Message;
 use App\Events\MessageEvent as Chat;
+use App\Events\ChatBoxEvent;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use App\Models\ChatBox;
@@ -14,6 +15,8 @@ class MessageController extends Controller
 {
     function sendChat(Request $req)
     {
+        event(new ChatBoxEvent($req->sender, $req->customer, $req->store));
+        event(new Chat($req->customer, $req->store, $req->sender, $req->content));
         return ["success" => "send success"];
     }
     //
@@ -33,9 +36,8 @@ class MessageController extends Controller
         //Ad message in messages
         $message = new Message();
         $message->uuid = Str::uuid();
-        $message->customer = $req->customer;
-        $message->store = $req->store;
         $message->sender = $req->sender;
+        $message->date = date("Y-m-d H:i:s");
         $message->content = $req->content;
 
         $message->save();
@@ -58,6 +60,7 @@ class MessageController extends Controller
             $chatBox->customer = $req->customer;
             $chatBox->store = $req->store;
             $chatBox->save();
+            event(new ChatBoxEvent($req->sender, $req->customer, $req->store));
         }
 
         $storeStatus->save();
@@ -78,6 +81,7 @@ class MessageController extends Controller
             $chatBox->customer = $req->customer;
             $chatBox->store = $req->store;
             $chatBox->save();
+            event(new ChatBoxEvent($req->sender, $req->customer, $req->store));
         }
 
         $customerStatus->save();
@@ -109,69 +113,68 @@ class MessageController extends Controller
 
     function getChatList(Request $req)
     {
-        if($req->owner == 'customer') {
+        if ($req->owner == 'customer') {
             $chatlist = ChatBox::select([
                 'chatboxes.*',
                 'stores.store_name as name',
                 'stores.image_uri as profile_uri'
             ])
-            ->where([
-                ['customer',$req->user],
-                ['owner_type', 'customer']
-            ])
-            ->join('stores','stores.uuid','chatboxes.store')
-            ->get();
-        }
-        else {
+                ->where([
+                    ['customer', $req->user],
+                    ['owner_type', 'customer']
+                ])
+                ->join('stores', 'stores.uuid', 'chatboxes.store')
+                ->get();
+        } else {
             $chatlist = ChatBox::select([
                 'chatboxes.*',
                 DB::raw('CONCAT(customers.fname," ",customers.lname) as name'),
                 'customers.profile_uri'
             ])
-            ->where([
-                ['store',$req->user],
-                ['owner_type', 'store']
-            ])
-            ->join('customers','customers.uuid','chatboxes.customer')
-            ->get();
+                ->where([
+                    ['store', $req->user],
+                    ['owner_type', 'store']
+                ])
+                ->join('customers', 'customers.uuid', 'chatboxes.customer')
+                ->get();
         }
 
         return $chatlist;
     }
 
-    function getMessages(Request $req) {
+    function getMessages(Request $req)
+    {
 
-        if($req->owner == 'customer') {
+        if ($req->owner == 'customer') {
             $messages = Message::select([
                 'messages.*',
                 'message_status.chatbox',
                 'message_status.status'
             ])
-            ->leftJoin('message_status','message_status.message','messages.uuid')
-            ->leftJoin('chatboxes','chatboxes.uuid','message_status.chatbox')
-            ->where([
-                ['chatboxes.customer',$req->user],
-                ['chatboxes.owner_type','customer']
-            ])
-            ->orderBy('messages.date', 'DESC')
-            ->get()
-            ->groupBy('chatbox');
-        }
-        else {
+                ->leftJoin('message_status', 'message_status.message', 'messages.uuid')
+                ->leftJoin('chatboxes', 'chatboxes.uuid', 'message_status.chatbox')
+                ->where([
+                    ['chatboxes.customer', $req->user],
+                    ['chatboxes.owner_type', 'customer']
+                ])
+                ->orderBy('messages.date', 'DESC')
+                ->get()
+                ->groupBy('chatbox');
+        } else {
             $messages = Message::select([
                 'messages.*',
                 'message_status.chatbox',
                 'message_status.status'
             ])
-            ->leftJoin('message_status','message_status.message','messages.uuid')
-            ->leftJoin('chatboxes','chatboxes.uuid','message_status.chatbox')
-            ->where([
-                ['chatboxes.store',$req->user],
-                ['chatboxes.owner_type','store']
-            ])
-            ->orderBy('messages.date', 'DESC')
-            ->get()
-            ->groupBy('chatbox');
+                ->leftJoin('message_status', 'message_status.message', 'messages.uuid')
+                ->leftJoin('chatboxes', 'chatboxes.uuid', 'message_status.chatbox')
+                ->where([
+                    ['chatboxes.store', $req->user],
+                    ['chatboxes.owner_type', 'store']
+                ])
+                ->orderBy('messages.date', 'DESC')
+                ->get()
+                ->groupBy('chatbox');
         }
 
         return $messages->toArray();
