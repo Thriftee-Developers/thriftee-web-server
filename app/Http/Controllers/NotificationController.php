bbc08b90bde6dcb8f4f9a4719155e2f87f529c3a
Marjon Magadia <marjonmagadia991@gmail.com>
2022-04-08 14:32:21 +0800

    u - optimize notification

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -50,7 +50,7 @@ class NotificationController extends Controller
         if ($req->type == "store") {
             return StoreNotification::where("store", $req->uuid)->orderBy(array("status" => "desc", "date" => "desc"))->orderByget();
         } else {
-            return CustomerNotification::where("customer", $req->uuid)->orderBy(["date", "desc"], ["status", "desc"])->get();
+            return CustomerNotification::where("customer", $req->uuid)->get()->orderBy(["date", "desc"], ["status", "desc"]);
         }
         return ["error" => "Customer or store not found!"];
     }
