3d762097fbde2a54bf2592431a1c82935cd347b2
Jet Aimemo <marjonmagadia991@gmail.com>
2022-04-30 01:08:32 +0800

    show list name

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -79,7 +79,7 @@ class SalesController extends Controller
             ) mBids
             ON mBids.bidding = biddings.uuid
 
-            WHERE ( biddings.status $status AND (biddings.end_time >= '$start_date' AND biddings.end_time <= '$end_date') OR stores.store_name LIKE '%$value$' OR categories.name LIKE '%$value%')
+            WHERE ( biddings.status $status AND ((biddings.end_time >= '$start_date' AND biddings.end_time <= '$end_date') OR stores.store_name LIKE '%$value$' OR categories.name LIKE '%$value%'))
             GROUP BY productcategories.product
             "
         );
